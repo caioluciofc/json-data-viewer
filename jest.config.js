b39c6fc34ede71206e83dc35bdcd5775251cde29
caioluciofc <caioluciofc@gmail.com>
2023-12-25 19:53:42 -0300

    Fix broken jest config

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    preset: "ts-jest",
+    transform: {
+      "^.+\\.ts?$": "ts-jest",
+    },
+    testEnvironment: "jest-environment-jsdom",
+    moduleDirectories: ["node_modules", "<rootDir>"],
+    // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+    moduleNameMapper: {
+      // Handle module aliases (this will be automatically configured for you soon)
+      "^@/components/(.*)$": "<rootDir>/components/$1",
+  
+      "^@/(.*)$": "<rootDir>/$1",
+      "^@/pages/(.*)$": "<rootDir>/pages/$1",
+      "^@/services/(.*)$": "<rootDir>/services/$1",
+    },
+  };
+  
