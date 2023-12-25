866ea6a913cb35a148c5f2a1d06ac51485151bcf
caioluciofc <caioluciofc@gmail.com>
2023-12-25 16:04:05 -0300

    Create title constants

---
+++ design_system/titles/small-title.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Colors, FontSizes } from '../_constants';
+
+interface Props {
+    text: string;
+    color?: string;
+    textAlign?: 'center' | 'left' | 'right';
+}
+
+export function TitleSmall({text, color = Colors.black, textAlign} : Props) {
+    return <span style={{color, textAlign, fontSize : FontSizes.small}}>{text}</span>;
+}
