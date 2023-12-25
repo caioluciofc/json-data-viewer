9967178033ea0c82e2ca5e39ff90a3ab0b8a21f6
caioluciofc <caioluciofc@gmail.com>
2023-12-25 15:54:13 -0300

    Create design system constants

---
+++ design_system/_constants/colors.ts
@@ -0,0 +1,53 @@
+/**
+ # JsonDataViewer Colors
+  | Primary | Secondary | Grey | DarkGrey | Background | Alert | Danger |
+  | -------- | ------- | -------- | ------- | -------- | ------- | -------- |
+  | ![](https://placehold.co/100x100/291F6C/291F6C.png) | ![](https://placehold.co/100x100/6C63FF/6C63FF.png) | ![](https://placehold.co/100x100/5D5D61/5D5D61.png) | ![](https://placehold.co/100x100/49484C/49484C.png) | ![](https://placehold.co/100x100/F7FAFC/F7FAFC.png) | ![](https://placehold.co/100x100/DC8926/DC8926.png) | ![](https://placehold.co/100x100/D13C3C/D13C3C.png) |
+  | #291F6C | #6C63FF | #5D5D61 | #49484C | #F7FAFC | #DC8926 | #D13C3C |
+*/
+export class Colors {
+    /**
+     ![](https://placehold.co/100x100/291F6C/291F6C.png)
+     */
+    static primary = '#291F6C';
+    /**
+      ![](https://placehold.co/100x100/6C63FF/6C63FF.png)
+     */
+    static secondary = '#6C63FF';
+    /**
+     ![](https://placehold.co/100x100/5D5D61/5D5D61.png)
+     */
+    static grey = '#5D5D61';
+    /**
+     ![](https://placehold.co/100x100/49484C/49484C.png)
+     */
+    static darkgrey = '#49484C';
+    /**
+     ![](https://placehold.co/100x100/F7FAFC/F7FAFC.png)
+     */
+    static background = '#F7FAFC'
+    /**
+     ![](https://placehold.co/100x100/DC8926/DC8926.png)
+     */
+     static alert = '#DC8926';
+     /**
+      ![](https://placehold.co/100x100/D13C3C/D13C3C.png)
+      */
+     static danger = '#D13C3C';
+     /**
+      ![](https://placehold.co/100x100/333333/333333.png)
+      */
+     static black = '#333333';
+     /**
+      ![](https://placehold.co/100x100/ffffff/ffffff.png)
+      */
+     static white = '#ffffff';
+     /**
+      ![](https://placehold.co/100x100/3293D4/3293D4.png)
+      */
+     static blue = '#3293D4';
+     /**
+      ![](https://placehold.co/100x100/49DE80/49DE80.png)
+      */
+     static green = '#49DE80';
+}
---
+++ design_system/_constants/fonts.ts
@@ -0,0 +1,37 @@
+/**
+  # JsonDataViewer App Font Sizes
+
+  <br/>
+
+  - FontSize.large = 20
+  - FontSize.medium = 16
+  - FontSize.small = 14
+
+  <br/>
+
+  ## How to use?
+
+  
+JavaScript
+
+  const styles = StyleSheet.create({
+    medium: {
+      fontSize: FontSize.medium,
+    },
+  });
+  
+*/
+export class FontSizes {
+    /**
+      # large === 20
+     */
+    static large = 20;
+    /**
+      ## medium === 16
+     */
+    static medium = 16;
+    /**
+      ### small === 14
+     */
+    static small = 14;
+   }
